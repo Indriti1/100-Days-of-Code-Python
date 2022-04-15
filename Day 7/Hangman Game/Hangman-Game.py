14978d3d43082e2cbb305fa3194d05aae6d5c004
Indrit Caca <cacaindrit81@gmail.com>
2022-04-16 00:06:49 +0200

    Day 7

---
+++ Day 7/Hangman Game/Hangman-Game.py
@@ -0,0 +1,50 @@
+#Step 5
+
+import replit
+import random
+import hangman_art as art
+import hangman_words as words
+
+chosen_word = random.choice(words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(art.logo)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    replit.clear()
+
+    if guess in display:
+        print(f"You have already guessed {guess}")
+    else:
+
+        for position in range(word_length):
+            letter = chosen_word[position]
+            if letter == guess:
+                display[position] = letter
+
+        if guess not in chosen_word:
+
+            print(
+                f"You guessed {guess}, that is not in the word. You lose a life."
+            )
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(art.stages[lives])
